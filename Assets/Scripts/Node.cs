69db259e5589a2566309b8993bb916ab38040734
Adam Graham <adam@zigurous.com>
2022-01-27 21:24:10 -0600

    Change float code formatting style

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -20,7 +20,7 @@ public class Node : MonoBehaviour
 
     private void CheckAvailableDirection(Vector2 direction)
     {
-        RaycastHit2D hit = Physics2D.BoxCast(transform.position, Vector2.one * 0.5f, 0.0f, direction, 1.0f, obstacleLayer);
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position, Vector2.one * 0.5f, 0f, direction, 1f, obstacleLayer);
 
         // If no collider is hit then there is no obstacle in that direction
         if (hit.collider == null) {
