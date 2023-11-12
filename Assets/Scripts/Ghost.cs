a7bade5dc4e174cdc4b74e31595ab33874193be7
Adam Graham <adam@zigurous.com>
2023-11-11 19:37:46 -0600

    Add ghost sprites and prefabs

---
+++ Assets/Scripts/Ghost.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Ghost : MonoBehaviour
+{
+    public Sprite[] spritesUp;
+    public Sprite[] spritesDown;
+    public Sprite[] spritesLeft;
+    public Sprite[] spritesRight;
+
+}
