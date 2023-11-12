6ea39b7c5565377a9217403496531c421e093cd9
Adam Graham <adam@zigurous.com>
2023-11-11 19:37:45 -0600

    Add Pellet and PowerPellet scripts

---
+++ Assets/Scripts/Pellet.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Pellet : MonoBehaviour
+{
+    public int points;
+
+    protected virtual void Eat()
+    {
+        this.gameObject.SetActive(false);
+
+        FindObjectOfType<GameManager>().IncreaseScore(this.points);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Pacman")) {
+            Eat();
+        }
+    }
+
+}
---
+++ Assets/Scripts/PowerPellet.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class PowerPellet : Pellet
+{
+    protected override void Eat()
+    {
+        base.Eat();
+
+        // TODO
+    }
+
+}
